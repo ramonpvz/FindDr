64e1d16e01c0de471b9c7ea1976ab87c1741122f
Ramon <ramon.pinedav@gmail.com>
2014-11-03 16:26:34 -0600

    First commit

---
+++ FindDr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindDr
+//
+//  Created by GLBMXM0002 on 11/3/14.
+//  Copyright (c) 2014 Find Doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FindDr/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FindDr
+//
+//  Created by GLBMXM0002 on 11/3/14.
+//  Copyright (c) 2014 Find Doctor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
