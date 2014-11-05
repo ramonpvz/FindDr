21021fcbdaace21d2285a71ca39d6dd8c013f2a2
eduardo milpas diaz <eduardo.milpas@globant.com>
2014-11-05 15:35:16 -0600

    changing look and feel

---
+++ code/FindDr/FindDr/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  FindDr
+//
+//  Created by eduardo milpas diaz on 11/5/14.
+//  Copyright (c) 2014 FindDr. All rights reserved.
+//
+
+class LoginViewController : UIViewController {
+
+    //MARK - View Properties
+    @IBOutlet var txtUser: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+
+
+    //MARK - Overriden properties
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //txtUser.layer.borderColor = UIColor(rgba: "#FFFFFF").CGColor
+    }
+
+}
+
+
