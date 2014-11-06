1def5c8a9e7fdf1d545733c0bdcc531d0e932043
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-11-06 13:18:38 -0600

    Added Location manager and map framework

--- code/FindDr/FindDr/MapViewViewController.m
+++ code/FindDr/FindDr/MapViewViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "MapViewViewController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
-@interface MapViewViewController ()
+@interface MapViewViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property CLLocationManager *myLocationManager;
+@property CLPlacemark *currentLocation;
 
 @end
 
@@ -16,12 +22,82 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.myLocationManager = [[CLLocationManager alloc] init];
+    [self.myLocationManager requestWhenInUseAuthorization];
+    self.myLocationManager.delegate = self;
+
+    [self.myLocationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Location Manager
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    for (CLLocation *location in locations) {
+        if(location.verticalAccuracy < 1000 && location.horizontalAccuracy < 1000){
+            [self reverseGeocode:location];
+            NSLog(@"location: %@",location);
+            [self.myLocationManager stopUpdatingLocation];
+            break;
+        }
+    }
+}
+
+- (void)reverseGeocode:(CLLocation *)location{
+    CLGeocoder *geocoder = [CLGeocoder new];
+    [geocoder reverseGeocodeLocation:location completionHandler:^(NSArray *placemarks, NSError *error) {
+        self.currentLocation = placemarks.firstObject;
+        NSString *address = [NSString stringWithFormat:@"%@, %@ at %@",
+                             self.currentLocation.subThoroughfare,
+                             self.currentLocation.thoroughfare,
+                             self.currentLocation.locality];
+        NSLog(@"Found you: %@",address);
+        [self zoomIn];
+    }];
+}
+
+#pragma mark - Map view
+- (void)zoomIn{
+    CLLocation *location = self.currentLocation.location;
+
+    CLLocationCoordinate2D zoom;
+    zoom.latitude = location.coordinate.latitude;
+    zoom.longitude = location.coordinate.longitude;
+
+    MKCoordinateSpan span;
+    span.latitudeDelta = .01;
+    span.longitudeDelta = .01;
+
+    MKCoordinateRegion region;
+    region.center = zoom;
+    region.span = span;
+    [self.mapView setRegion:region animated:YES];
+    [self.mapView regionThatFits:region];
+}
+
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"MyPinID"];
+    pin.canShowCallout = YES;
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    pin.image = [UIImage imageNamed:@"clinic.png"];
+
+    return pin;
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                         message:@"Failed to Get Your Location"
+                                                        delegate:nil
+                                               cancelButtonTitle:@"OK"
+                                               otherButtonTitles:nil];
+    [errorAlert show];
+    NSLog(@"Error: %@",error);
 }
 
 /*
